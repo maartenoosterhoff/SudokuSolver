564ac2145582feffa5522bfaa1e56495f253d7a4
Maarten Oosterhoff <maartenoosterhoff@funda.nl>
2017-06-22 11:25:11 +0200

    Clearing up.

--- src/SudokuSolver/SudokuSolver.Core/Solvers/Techniques/AlignedPairExclusionSolvingTechnique.cs
+++ src/SudokuSolver/SudokuSolver.Core/Solvers/Techniques/AlignedPairExclusionSolvingTechnique.cs
@@ -15,7 +15,8 @@ namespace SudokuSolver.Core.Solvers.Techniques
                             let A = proxy.GroupAsBitSet(groupA.Id)
                             let B = proxy.GroupAsBitSet(groupB.Id)
                             let inLayer = A & B
-                            let outLayer = (A | B) & (!inLayer)
+                            //let outLayer = (A | B) & (!inLayer)
+                            let outLayer = A ^ B
                             let solution = SolveInternal(proxy, inLayer, outLayer)
                             where solution != null
                             select solution;
@@ -44,6 +45,7 @@ namespace SudokuSolver.Core.Solvers.Techniques
                                 value1.Add(v1);
                                 value2.Add(v2);
                             }
+
                 // Remove all combinations which are taken by the surrounding cells
                 for (var lp = 0; lp < outLayer.Size; lp++)
                 {
@@ -73,53 +75,37 @@ namespace SudokuSolver.Core.Solvers.Techniques
                 }
 
                 // Update the paircells.
-                var p1t = string.Empty;
-                var p2t = string.Empty;
                 var stepTaken = false;
-                var solveStepItems = Enumerable.Range(0, proxy.SudokuBoard.CandidateCount).ToDictionary(i => i, i => new SolveStepItem() { Value = i, TechniqueName = "Aligned Pair Exclusion", SolveStepType = SolveStepItemType.CandidateRemoval });
+                var solveStepItems = Enumerable.Range(0, proxy.SudokuBoard.CandidateCount).ToDictionary(i => i, i => new List<int>());
                 for (var i = 0; i < proxy.SudokuBoard.CandidateCount; i++)
                 {
                     if (c1.Candidates[i] && !value1.Contains(i))
                     {
-                        var cellIds = (solveStepItems[i].CellIds ?? new int[0]).ToList();
-                        cellIds.Add(cell1[pos]);
-                        solveStepItems[i].CellIds = cellIds.ToArray();
-                        //proxy.SudokuBoard.Cells[cell1[pos]].Candidates[i] = false;
+                        solveStepItems[i].Add(cell1[pos]);
                         stepTaken = true;
-                        //if (!string.IsNullOrEmpty(p1t)) p1t += ",";
-                        //p1t += Candidate.PrintValue(i);
                     }
                     if (c2.Candidates[i] && !value2.Contains(i))
                     {
-                        var cellIds = (solveStepItems[i].CellIds ?? new int[0]).ToList();
-                        cellIds.Add(cell2[pos]);
-                        solveStepItems[i].CellIds = cellIds.ToArray();
-                        //proxy.SudokuBoard.Cells[cell2[pos]].Candidates[i] = false;
-                        //if (!string.IsNullOrEmpty(p1t)) p2t += ",";
-                        //p2t += Candidate.PrintValue(i);
+                        solveStepItems[i].Add(cell2[pos]);
                     }
                 }
 
                 if (stepTaken)
                 {
-                    var items = solveStepItems.Select(x => x.Value).Where(x => x.CellIds != null).ToArray();
-                    foreach (var item in items)
-                    {
-                        item.Explanation = $"Found an Aligned Pair Exclusion in cells {string.Join(",", item.CellIds.Select(x => proxy.SudokuBoard.Cells[x].Name))}, removing candidate {item.Value}";
-                    }
+                    var items = from x in solveStepItems
+                                where x.Value.Count > 0
+                                select new SolveStepItem
+                                {
+                                    Value = x.Key,
+                                    SolveStepType = SolveStepItemType.CandidateRemoval,
+                                    CellIds = x.Value.ToArray(),
+                                    TechniqueName = "Aligned Pair Exclusion",
+                                    Explanation = $"Found an Aligned Pair Exclusion in cells {string.Join(",", x.Value.Select(c => proxy.SudokuBoard.Cells[c].Name))}, removing candidate {Candidate.PrintValue(x.Key)}"
+                                };
                     var solveStep = new SolveStep()
                     {
-                        Items = items
+                        Items = items.ToArray()
                     };
-                    //step.Description = "Found an Aligned Pair Exclusion in cells " + c1.Name + " and " + c2.Name + " - removing candidates ";
-                    //if (!string.IsNullOrEmpty(p1t))
-                    //    step.Description += p1t + " from cell " + c1.Name;
-                    //if (!string.IsNullOrEmpty(p1t) && !string.IsNullOrEmpty(p2t))
-                    //    step.Description += " and ";
-                    //if (!string.IsNullOrEmpty(p2t))
-                    //    step.Description += p2t + " from cell " + c2.Name;
-                    //step.Description += "\r\n";
-
                     return solveStep;
                 }
             }
